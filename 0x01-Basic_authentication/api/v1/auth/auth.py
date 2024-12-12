38af4d1f0f5d79cc4c82889b50afe1cc51cf1a75
Joumana Elsayed <joumanasalahedin@gmail.com>
2024-12-12 17:07:10 +0100

    task 5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -41,7 +41,10 @@ class Auth:
         Returns:
             str: None for now.
         """
-        return None
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
