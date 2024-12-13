c3a108be23b5d0262b95a2581320a53142951d78
Joumana Elsayed <joumanasalahedin@gmail.com>
2024-12-13 10:29:29 +0100

    task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ BasicAuth module.
 import base64
 from api.v1.auth.auth import Auth
 from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -85,13 +86,14 @@ class BasicAuth(Auth):
 
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        try:
-            users = User.search({"email": user_email})
-            if not users or users == []:
-                return None
-            for u in users:
-                if u.is_valid_password(user_pwd):
-                    return u
+
+        users = User.search({'email': user_email})
+        if not users:
             return None
-        except Exception:
+
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
             return None
+
+        return user
