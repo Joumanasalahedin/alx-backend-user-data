2dee57417af7575ec37f12089fe59b49dde48859
Joumana Elsayed <joumanasalahedin@gmail.com>
2024-12-16 15:12:55 +0100

    pycodestyle edits

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -11,7 +11,7 @@ from api.v1.views import app_views
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login():
     """
-    Handles POST /auth_session/login to log in a user via session authentication.
+    Handles POST to log in a user via session authentication.
     """
     email = request.form.get('email')
     password = request.form.get('password')
