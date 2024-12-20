4c0da882723f2c36a5510de0a7e2dcfbfeafb5f9
Joumana Elsayed <joumanasalahedin@gmail.com>
2024-12-20 13:14:32 +0100

    task 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -106,3 +106,15 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Destroy a user's session by setting their session_id to None.
+
+        Returns:
+            None
+        """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            pass
