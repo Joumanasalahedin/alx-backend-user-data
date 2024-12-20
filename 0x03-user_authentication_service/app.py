a8eed061be3339180f032e390eb91ecf997bc70c
Joumana Elsayed <joumanasalahedin@gmail.com>
2024-12-20 14:13:38 +0100

    task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -122,5 +122,25 @@ def reset_password():
         abort(403)
 
 
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    """
+    Endpoint to update a user's password.
+
+    Returns:
+        - 200: user's email and a success message if the password was updated.
+        - 403: Error response if the reset token is invalid.
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
