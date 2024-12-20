f9bcec468cb2cff794d3d79120765b174a68ec1f
Joumana Elsayed <joumanasalahedin@gmail.com>
2024-12-20 14:03:03 +0100

    task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -132,7 +132,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
 
-            reset_token = self._generate_uuid()
+            reset_token = _generate_uuid()
 
             self._db.update_user(user.id, reset_token=reset_token)
 
